2a3d51e46b9ce0a0a6579b881e441a06fbdd24d4
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2021-12-10 12:48:19 +0100

    Add NAV design system

--- apps/innbyggerpanelet/src/app/app.tsx
+++ apps/innbyggerpanelet/src/app/app.tsx
@@ -1,26 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { Message } from '@innbyggerpanelet/api-interfaces';
+import React from 'react';
+import "@navikt/ds-css"
 
 export const App = () => {
-  const [m, setMessage] = useState<Message>({ message: '' });
-
-  useEffect(() => {
-    fetch('/api')
-      .then((r) => r.json())
-      .then(setMessage);
-  }, []);
-
   return (
     <>
-      <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to innbyggerpanelet!</h1>
-        <img
-          width="450"
-          src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
-          alt="Nx - Smart, Extensible Build Framework"
-        />
-      </div>
-      <div>{m.message}</div>
+      
     </>
   );
 };
