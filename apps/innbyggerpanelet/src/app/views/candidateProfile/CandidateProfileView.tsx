e77dd6402227610af6b1e38751ffb019c980f953
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2021-12-10 15:10:29 +0100

    Start work on candidates properties information

--- apps/innbyggerpanelet/src/app/views/candidateProfile/CandidateProfileView.tsx
+++ apps/innbyggerpanelet/src/app/views/candidateProfile/CandidateProfileView.tsx
@@ -1,15 +1,18 @@
 import { Button, Heading } from '@navikt/ds-react'
 import React, { ReactElement } from 'react'
 import CandidateProfileInfo from '../../components/candidateProfile/CandidateProfileInfo'
+import CandidateProperties from '../../components/candidateProfile/CandidateProperties'
 
 export default function CandidateProfileView(): ReactElement {
     return (
-        <div>
+        <div className="candidate-profile-view">
             <div className="candidate-view-header">
                 <Heading size="xlarge">Din profil</Heading>
                 <Button>Ta kontakt</Button>
             </div>
             <CandidateProfileInfo />
+            <Heading size="medium">Egenskaper</Heading>
+            <CandidateProperties />
         </div>
     )
 }
