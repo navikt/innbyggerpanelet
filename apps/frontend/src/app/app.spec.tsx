517150f81992b1e9d3177cfb68a52d7a5c421ac0
Amund Lunke Røhne <amund.lunke.rohne@nav.no>
2021-12-22 15:39:40 +0100

    refactor[frontend]: fix for issue with jest and @navikt/ds-css

--- apps/frontend/src/app/app.spec.tsx
+++ apps/frontend/src/app/app.spec.tsx
@@ -3,19 +3,19 @@ import React from 'react';
 import App from './app';
 
 describe('App', () => {
-  afterEach(() => {
-    delete global['fetch'];
-    cleanup();
-  });
-
-  it('should render successfully', async () => {
-    global['fetch'] = jest.fn().mockResolvedValueOnce({
-      json: () => ({
-        message: 'my message',
-      }),
+    afterEach(() => {
+        delete global['fetch'];
+        cleanup();
     });
 
-    const { baseElement } = render(<App />);
-    await waitFor(() => getByText(baseElement, 'my message'));
-  });
+    it('should render successfully', async () => {
+        global['fetch'] = jest.fn().mockResolvedValueOnce({
+            json: () => ({
+                message: 'my message',
+            }),
+        });
+
+        const { baseElement } = render(<App />);
+        await waitFor(() => getByText(baseElement, 'Nytt innsiktsarbeid'));
+    });
 });
--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { CreateInsight } from './containers/createInsight/CreateInsight';
-import '@navikt/ds-css';
-
-import './app.module.scss';
+import '@navikt/ds-css/index.css';
 
 export const App = () => {
     return (
