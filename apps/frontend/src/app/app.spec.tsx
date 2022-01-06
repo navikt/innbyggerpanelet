a2057329b851abb931c61c75e06b4e7d7c3a52bb
Amund Lunke Røhne <amund.lunke.rohne@nav.no>
2022-01-06 13:36:25 +0100

    fix[frontend]: add context to prevent react router from crashing tests

--- apps/frontend/src/app/app.spec.tsx
+++ apps/frontend/src/app/app.spec.tsx
@@ -1,5 +1,5 @@
 import { cleanup, getByText, render, waitFor } from '@testing-library/react';
-import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './app';
 
 describe('App', () => {
