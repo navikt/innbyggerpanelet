a6c372d88c20742c60de5fc9b8b9cb885b2d4f81
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-01-27 14:15:24 +0100

    wip

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -5,6 +5,12 @@ import insightRoutes from './InsightRoutes';
 import criteriaRoutes from './CriteriaRoutes';
 
 const routes = Router()
+    .use('/isAlive', (req, res) => {
+        res.status(200)
+    })
+    .use('/isReady', (req, res) => {
+        res.status(200)
+    })
     .use('/candidate', candidateRoutes)
     .use('/consent', consentRoutes)
     .use('/insight', insightRoutes)
