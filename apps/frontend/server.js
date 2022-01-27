23aad4865fdb7d23c0b79c37b05188ea089adf17
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-01-27 10:52:43 +0100

    Add status checks for frontend prod server

--- apps/frontend/server.js
+++ apps/frontend/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 const port = process.env.SERVER_PORT || 2022;
-const index = (req, res) => res.sendFile(require('path').join(__dirname, 'dist/index.html'))
+const index = (req, res) => res.sendFile(require('path').join(__dirname, 'dist/index.html'));
 app.use(express.static('dist'));
 // Add routes here
-app.get("/", index);
+app.get('/', index);
+
+app.get('/isalive', (req, res) => {
+    res.sendStatus(200);
+});
+
+app.get('/isready', (req, res) => {
+    res.sendStatus(200);
+});
 
 app.listen(port, () => console.log(`Server starting on port ${port}`));
