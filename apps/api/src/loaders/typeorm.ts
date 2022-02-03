ef182a8c86748e71b35f08ef9e9a78406e09431e
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-02-03 20:23:07 +0100

    Testy test

--- apps/api/src/loaders/typeorm.ts
+++ apps/api/src/loaders/typeorm.ts
@@ -8,11 +8,11 @@ export default async () => {
 
     const typeormConfig: ConnectionOptions = {
         type: 'postgres',
-        host: config.database.host,
-        port: Number(config.database.port),
-        database: config.database.db,
-        username: config.database.user,
-        password: config.database.password,
+        host: process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_API_DB_HOST,
+        port: Number(process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_API_DB_PORT),
+        database: process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_API_DB_DATABASE,
+        username: process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_API_DB_USERNAME,
+        password: process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_API_DB_PASSWORD,
         synchronize: true,
         entities: models,
     };
