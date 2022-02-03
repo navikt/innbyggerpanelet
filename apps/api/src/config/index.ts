0655f69d7d4a034a1be55165bce7c8fa77f71553
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-02-03 09:21:39 +0100

    Add configs for env's

---
+++ apps/api/src/config/index.ts
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv'
+
+const environment = process.env.NODE_ENV
+
+dotenv.config()
+
+let host
+let port
+let db
+let user
+let password
+if (environment === 'production') {
+    host = process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_DB_HOST
+    port = process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_DB_PORT
+    db = process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_DB_DATABASE
+    user = process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_DB_USER
+    password = process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_DB_PASSWORD
+} else {
+    host = process.env.DB_HOST
+    port = process.env.DB_PORT
+    db = process.env.POSTGRES_DB
+    user = process.env.POSTGRES_USER
+    password = process.env.POSTGRES_PASSWORD
+}
+
+export default {
+    database: {
+        host: host,
+        port: port,
+        db: db,
+        user: user,
+        password: password
+    },
+}
--- apps/api/src/loaders/typeorm.ts
+++ apps/api/src/loaders/typeorm.ts
@@ -1,14 +1,15 @@
 import { ConnectionOptions, createConnection } from 'typeorm';
+import config from '../config';
 import models from '../models';
 
 export default async () => {
     const typeormConfig: ConnectionOptions = {
         type: 'postgres',
-        host: process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_DB_HOST,
-        port: Number(process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_DB_PORT),
-        username: process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_DB_USER,
-        password: process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_DB_PASSWORD,
-        database: process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_DB_DATABASE,
+        host: config.database.host,
+        port: Number.parseInt(config.database.port!),
+        username: config.database.user,
+        password: config.database.password,
+        database: config.database.db,
         synchronize: true,
         entities: models,
     };
