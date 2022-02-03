b6cf5c4252846b5b14891f0ddf8d4115814d93e8
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-02-03 18:12:38 +0100

    Try new env's

--- apps/api/src/loaders/typeorm.ts
+++ apps/api/src/loaders/typeorm.ts
@@ -5,11 +5,11 @@ import models from '../models';
 export default async () => {
     const typeormConfig: ConnectionOptions = {
         type: 'postgres',
-        host: config.database.host,
-        port: Number.parseInt(config.database.port!),
-        database: config.database.db,
-        username: config.database.user,
-        password: config.database.password,
+        host: process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_API_DB_HOST,
+        port: Number(process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_API_DB_PORT),
+        database: process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_API_DB_DATABASE,
+        username: process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_API_DB_USERNAME,
+        password: process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_API_DB_PASSWORD,
         synchronize: true,
         entities: models,
     };
