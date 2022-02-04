8bce84127de02541773ccd64cbc4b044149c6938
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-02-04 10:02:48 +0100

    What am i even doing at this point

--- apps/api/src/loaders/typeorm.ts
+++ apps/api/src/loaders/typeorm.ts
@@ -6,11 +6,11 @@ import dotenv from 'dotenv'
 export default async () => {
     const typeormConfig: ConnectionOptions = {
         type: 'postgres',
-        host: '127.0.0.1',
-        port: 5432,
-        database: 'innbyggerpanelet-dbs',
-        username: 'innbyggerpanelet-api',
-        password: process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_DBS_PASSWORD,
+        host: process.env.NAIS_DATABASE_INNBYGGERPANELET_BACKEND_INNBYGGERPANELET_BACKEND_DB_HOST,
+        port: Number(process.env.NAIS_DATABASE_INNBYGGERPANELET_BACKEND_INNBYGGERPANELET_BACKEND_DB_PORT),
+        database: process.env.NAIS_DATABASE_INNBYGGERPANELET_BACKEND_INNBYGGERPANELET_BACKEND_DB_DATABASE,
+        username: process.env.NAIS_DATABASE_INNBYGGERPANELET_BACKEND_INNBYGGERPANELET_BACKEND_DB_USERNAME,
+        password: process.env.NAIS_DATABASE_INNBYGGERPANELET_BACKEND_INNBYGGERPANELET_BACKEND_DB_PASSWORD,
         synchronize: true,
         entities: models,
     };
