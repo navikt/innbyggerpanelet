4e88bf8784d1dd932e4b71de960f27477f3dda3d
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-02-04 09:46:02 +0100

    Try manual envs

--- apps/api/src/loaders/typeorm.ts
+++ apps/api/src/loaders/typeorm.ts
@@ -6,10 +6,10 @@ import dotenv from 'dotenv'
 export default async () => {
     const typeormConfig: ConnectionOptions = {
         type: 'postgres',
-        host: process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_DBS_HOST,
-        port: Number(process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_DBS_PORT!),
-        database: process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_DBS_DATABASE,
-        username: process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_DBS_USERNAME,
+        host: '127.0.0.1',
+        port: 5432,
+        database: 'innbyggerpanelet-dbs',
+        username: 'innbyggerpanelet-api',
         password: process.env.NAIS_DATABASE_INNBYGGERPANELET_API_INNBYGGERPANELET_DBS_PASSWORD,
         synchronize: true,
         entities: models,
