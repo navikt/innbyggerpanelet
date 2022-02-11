af8d50a0ac7e75bf5965362fa3a135936c84f695
Amund Lunke Røhne <amund.lunke.rohne@nav.no>
2022-02-11 15:42:01 +0100

    fix[deploy]: frontend api proxy

--- apps/frontend/server.js
+++ apps/frontend/server.js
@@ -1,28 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 2022
-const path = require('path')
+const express = require('express');
+const app = express();
+const port = 2022;
+const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 // Setter static folder
 
-app.use(express.static(path.join(__dirname, '../../dist/apps/frontend')))
+app.use(express.static(path.join(__dirname, '../../dist/apps/frontend')));
 
+app.use('/api', createProxyMiddleware({ target: process.env.API_URL, changeOrigin: true }));
 // app.get('/', (req, res) => {
 //     res.send('Hello World!')
 // })
 
 app.get('/isalive', (req, res) => {
-    res.sendStatus(200)
-})
+    res.sendStatus(200);
+});
 
 app.get('/isready', (req, res) => {
-    res.sendStatus(200)
-})
+    res.sendStatus(200);
+});
 
-app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, '../../dist/apps/frontend', 'index.html'))
-})
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../dist/apps/frontend', 'index.html'));
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
