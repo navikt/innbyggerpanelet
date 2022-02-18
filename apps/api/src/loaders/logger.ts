d1e0ee5a136135f8328856cb34c99a9e17761e7b
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-02-18 11:36:13 +0100

    Add general logger

---
+++ apps/api/src/loaders/logger.ts
@@ -0,0 +1,32 @@
+import winston from 'winston';
+
+const dateFormat = 'DD-MM-YYYY - HH:mm:ss';
+
+const transports = [];
+
+transports.push(
+    new winston.transports.Console({
+        format: winston.format.combine(winston.format.cli(), winston.format.splat())
+    }),
+    new winston.transports.File({
+        filename: 'logs/error.log',
+        level: 'error'
+    }),
+    new winston.transports.File({
+        filename: 'logs/all.log'
+    })
+);
+
+const logger = winston.createLogger({
+    format: winston.format.combine(
+        winston.format.timestamp({
+            format: dateFormat
+        }),
+        winston.format.errors({ stack: true }),
+        winston.format.splat(),
+        winston.format.json()
+    ),
+    transports
+});
+
+export { logger };
