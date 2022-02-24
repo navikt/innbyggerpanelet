51079d48220bed44b07c6bedc2deb7dd3e14738e
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-02-24 14:25:09 +0100

    Update from Browser routes to Hash routes

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import App from './app/app';
 import UserProfile from './app/containers/userProfile';
 import CreateInsight from './app/containers/createInsight/';
@@ -15,7 +15,7 @@ import RegisterUser from './app/containers/registerUser';
 // Root
 ReactDOM.render(
     <StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <Header />
             <Breadcrumbs />
             <Routes>
@@ -29,7 +29,7 @@ ReactDOM.render(
                 <Route path="/admin/kriterier" element={<CriteriaAdminPanel />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>,
     document.getElementById('root')
 );
