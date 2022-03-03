0b97d30392ddb0013557477ed797558afe0e04aa
Amund Lunke Røhne <amund.lunke.rohne@nav.no>
2022-03-03 16:10:25 +0100

    feat[frontend]: NAV date picker for insight project creation

--- apps/frontend/src/app/components/project/ProjectEdit.tsx
+++ apps/frontend/src/app/components/project/ProjectEdit.tsx
@@ -1,9 +1,10 @@
-import { ChangeEvent, ReactElement } from 'react';
 import { IInsightProject } from '@innbyggerpanelet/api-interfaces';
-import { Button, Textarea, TextField } from '@navikt/ds-react';
-
-import style from './Project.module.scss';
+import { Datepicker } from '@navikt/ds-datepicker';
+import { DatepickerValue } from '@navikt/ds-datepicker/lib/Datepicker';
+import { BodyShort, Button, Label, Textarea, TextField } from '@navikt/ds-react';
+import { ChangeEvent, ReactElement } from 'react';
 import ProjectTeam from '../projectTeam';
+import style from './Project.module.scss';
 
 interface IProps {
     project: IInsightProject;
@@ -12,53 +13,39 @@ interface IProps {
     loading: boolean;
 }
 
-export const ProjectEdit = ({
-    project,
-    setProject,
-    submit,
-    loading,
-}: IProps): ReactElement => {
-    const handleInputChange = (
-        event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-    ) => {
+export const ProjectEdit = ({ project, setProject, submit, loading }: IProps): ReactElement => {
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const newProject = { ...project };
         newProject[event.target.id] = event.target.value;
         setProject(newProject);
     };
 
+    const handleDateChange = (value: DatepickerValue, id: string) => {
+        const newProject = { ...project };
+        newProject[id] = value;
+        setProject(newProject);
+    };
+
     return (
         <form className={style.projectEdit}>
-            <TextField
-                id="name"
-                label="Navn:"
-                value={project.name}
-                onChange={handleInputChange}
-            />
-            <Textarea
-                id="description"
-                label="Beskrivelse:"
-                value={project.description}
-                onChange={handleInputChange}
-            />
-            <TextField
-                id="start"
-                label="Startdato:"
-                value={project.start}
-                onChange={handleInputChange}
-            />
-            <TextField
-                id="end"
-                label="Sluttdato:"
-                value={project.end}
-                onChange={handleInputChange}
-            />
+            <TextField id="name" label="Navn:" value={project.name} onChange={handleInputChange} />
+            <Textarea id="description" label="Beskrivelse:" value={project.description} onChange={handleInputChange} />
+            <div>
+                <Label>Innsiktsperiode</Label>
+                <div className={style.dates}>
+                    <Datepicker value={project.start} onChange={(value) => handleDateChange(value, 'start')} />
+                    <BodyShort>til</BodyShort>
+                    <Datepicker value={project.end} onChange={(value) => handleDateChange(value, 'end')} />
+                </div>
+            </div>
             <ProjectTeam project={project} edit={setProject} />
             <Button
                 loading={loading}
                 onClick={(e) => {
                     e.preventDefault();
                     submit(project);
-                }}>
+                }}
+            >
                 Bekreft
             </Button>
         </form>
