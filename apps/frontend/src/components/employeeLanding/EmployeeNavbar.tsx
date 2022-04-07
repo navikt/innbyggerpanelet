f6022997f3c7f0a0fb7e80c52286031129375d94
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-04-07 10:54:44 +0200

    Add employee info container

--- apps/frontend/src/components/employeeLanding/EmployeeNavbar.tsx
+++ apps/frontend/src/components/employeeLanding/EmployeeNavbar.tsx
@@ -1,3 +1,4 @@
+import { Email } from '@navikt/ds-icons';
 import { Link, Button } from '@navikt/ds-react';
 import React, { ReactElement } from 'react';
 import style from './EmployeeNavbar.module.scss';
@@ -8,7 +9,7 @@ export default function EmployeeNavbar(): ReactElement {
             <Link href="#/profil">
                 Min profil
             </Link>
-            <svg width="1.75em" height="1.75em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" focusable="false" role="img"><path fill-rule="evenodd" clip-rule="evenodd" d="M2 5h20v1.764l-10 5-10-5V5zM0 5a2 2 0 012-2h20a2 2 0 012 2v13a3 3 0 01-3 3H3a3 3 0 01-3-3V5zm2 4v9a1 1 0 001 1h18a1 1 0 001-1V9l-10 5L2 9z" fill="currentColor"></path></svg>
+            <Email width={'1.75rem'} height={'1.75rem'}/>
             <div>
                 <Button variant="primary" size="medium">
                 Logg ut
--- apps/frontend/src/views/employeeLanding/EmployeeLandingPage.tsx
+++ apps/frontend/src/views/employeeLanding/EmployeeLandingPage.tsx
@@ -1,10 +1,15 @@
 import React, { ReactElement } from 'react';
+import EmployeeInfo from '../../components/employeeLanding/EmployeeInfo';
 import EmployeeNavbar from '../../components/employeeLanding/EmployeeNavbar';
+
 
 export default function EmployeeLandingPage(): ReactElement {
     return (
         <div>
             <EmployeeNavbar />
+            <div>
+                <EmployeeInfo name='Emil Elton Nilsen'/>
+            </div>
         </div>
     );
 }
