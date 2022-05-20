f5a5de369f0e01cc2e54cae6426c42a6345a0d6e
Amund Lunke Røhne <43012250+amundlrohne@users.noreply.github.com>
2022-05-20 09:37:00 +0200

    feat[frontend]: stacktrace in dev env (#101)

--- apps/api/src/routes/middleware/errorHandler.ts
+++ apps/api/src/routes/middleware/errorHandler.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { QueryFailedError } from 'typeorm';
+import config from '../../config';
 import { BaseError } from '../../lib/errors/BaseError';
 import { BadRequestError } from '../../lib/errors/http/BadRequestError';
 import { ServerErrorMessage } from '../../lib/errors/messages/ServerErrorMessages';
@@ -14,6 +15,11 @@ export function errorHandler(error: Error, request: Request, response: Response,
         const error = new BadRequestError({ message: ServerErrorMessage.invalidData() });
         return response.status(error.httpStatus).json(error.toResponse());
     }
+
+    if (!config.backend.prod)
+        return response
+            .status(StatusCodes.INTERNAL_SERVER_ERROR)
+            .json('Something went wrong.. Try again later. \n' + error);
 
     return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json('Something went wrong.. Try again later.');
 }
