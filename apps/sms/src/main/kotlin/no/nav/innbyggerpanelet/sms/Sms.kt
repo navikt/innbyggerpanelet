f210be0734dff4f053b508dfcd420ac36392d49e
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-06-08 10:54:29 +0200

    My first spring route

---
+++ apps/sms/src/main/kotlin/no/nav/innbyggerpanelet/sms/Sms.kt
@@ -0,0 +1,6 @@
+package no.nav.innbyggerpanelet.sms
+
+data class Sms(
+    val phoneNumber: String,
+    val message: String
+)
---
+++ apps/sms/src/main/kotlin/no/nav/innbyggerpanelet/sms/SmsController.kt
@@ -0,0 +1,14 @@
+package no.nav.innbyggerpanelet.sms
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+internal class SmsController {
+
+    @PostMapping("/sms")
+    fun sms(@RequestBody sms: Sms) {
+        println(sms)
+    }
+}
