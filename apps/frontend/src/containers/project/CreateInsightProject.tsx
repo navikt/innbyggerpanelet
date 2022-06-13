81007a182bf48eeaeac27f8d2b1a78577b286546
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-06-13 13:11:43 +0200

    Add file back

---
+++ apps/frontend/src/containers/project/CreateInsightProject.tsx
@@ -0,0 +1,45 @@
+import { IInsightProject } from '@innbyggerpanelet/api-interfaces';
+import { Panel } from '@navikt/ds-react';
+import { ReactElement, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { createInsightProject } from '../../api/mutations/mutateInsightProject';
+import { ProjectEdit } from '../../components/project';
+
+const defaultProject: IInsightProject = {
+    id: 0,
+    name: '',
+    description: '',
+    start: '',
+    end: '',
+    members: []
+};
+
+export const CreateInsightProject = (): ReactElement => {
+    const [insightProject, setInsightProject] = useState(defaultProject);
+    const [posting, setPosting] = useState(false);
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (project: IInsightProject) => {
+        const { response, isLoading, isError } = await createInsightProject(project);
+
+        if (response) {
+            navigate(`/prosjekt/${response.id}`);
+        } else if (isLoading) {
+            setPosting(true);
+        } else if (isError) {
+            console.error(isError);
+        }
+    };
+
+    return (
+        <Panel>
+            <ProjectEdit
+                project={insightProject}
+                setProject={setInsightProject}
+                submit={handleSubmit}
+                loading={posting}
+            />
+        </Panel>
+    );
+};
