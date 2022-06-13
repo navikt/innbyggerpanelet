19dbd4e688fd116d20f727864d2436e46f7007a3
Amund Lunke Røhne <amund.lunke.rohne@nav.no>
2022-06-13 12:37:28 +0200

    feat[frontend]: messages tab selector

--- apps/frontend/src/views/messages/MessagesPage.tsx
+++ apps/frontend/src/views/messages/MessagesPage.tsx
@@ -1,27 +1,57 @@
-import { ReactElement } from 'react';
+import { Email, ShakeHands } from '@navikt/ds-icons';
+import { BodyShort } from '@navikt/ds-react';
+import { ReactElement, useState } from 'react';
 import { useCandidatesByCurrentUser } from '../../api/hooks';
 import { useMessages } from '../../api/hooks/useMessages';
 import { APIHandler } from '../../components/misc/apiHandler';
 import { PanelNoBackground } from '../../components/misc/panelNoBackground';
+import PanelTabs from '../../components/misc/panelTabs/PanelTabs';
 import { Inbox } from '../../containers/messages/Inbox';
 import { Invitations } from '../../containers/messages/Invitations';
+
+enum EnumTabState {
+    Invitations = 'INVITATIONS',
+    Messages = 'MESSAGES'
+}
 
 export const MessagesPage = (): ReactElement => {
     const invitations = useCandidatesByCurrentUser();
     const inbox = useMessages();
 
+    const [tabState, setTabState] = useState<EnumTabState>(EnumTabState.Invitations);
+
     return (
         <PanelNoBackground>
-            {invitations.candidates ? (
-                <Invitations candidates={invitations.candidates} />
-            ) : (
-                <APIHandler loading={invitations.loading} error={invitations.error} />
-            )}
-            {inbox.messages ? (
-                <Inbox messages={inbox.messages} />
-            ) : (
-                <APIHandler loading={inbox.loading} error={inbox.error} />
-            )}
+            <PanelTabs>
+                <PanelTabs.TabSelector
+                    active={tabState === EnumTabState.Invitations}
+                    onClick={() => setTabState(EnumTabState.Invitations)}
+                >
+                    <ShakeHands />
+                    <BodyShort>Invitasjoner</BodyShort>
+                </PanelTabs.TabSelector>
+                <PanelTabs.TabSelector
+                    active={tabState === EnumTabState.Messages}
+                    onClick={() => setTabState(EnumTabState.Messages)}
+                >
+                    <Email />
+                    <BodyShort>Meldinger</BodyShort>
+                </PanelTabs.TabSelector>
+            </PanelTabs>
+            <PanelTabs.TabContent active={tabState === EnumTabState.Invitations}>
+                {invitations.candidates ? (
+                    <Invitations candidates={invitations.candidates} />
+                ) : (
+                    <APIHandler loading={invitations.loading} error={invitations.error} />
+                )}
+            </PanelTabs.TabContent>
+            <PanelTabs.TabContent active={tabState === EnumTabState.Messages}>
+                {inbox.messages ? (
+                    <Inbox messages={inbox.messages} />
+                ) : (
+                    <APIHandler loading={inbox.loading} error={inbox.error} />
+                )}
+            </PanelTabs.TabContent>
         </PanelNoBackground>
     );
 };
