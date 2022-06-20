a991566c2002496d51b22c361c7605784c2b942c
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-06-20 13:01:00 +0200

    Fix loggin bug

--- apps/api/src/routes/middleware/httpLogging.ts
+++ apps/api/src/routes/middleware/httpLogging.ts
@@ -5,16 +5,16 @@ import clfDate from 'clf-date';
 export function httpLogger(req: Request, res: Response, next: NextFunction) {
 
     const uuid = uuidv4();
-    // TODO add real user id
+    // TODO add real user
     const userId = 'userid';
     const remoteUser = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     const date = clfDate(new Date());
     const { method, url, httpVersion } = req;
     const { statusCode } = res;
 
-    const contentLength = res.json()['_contentLength'] | 0;
+    const contentLength = res.get('content-length') || '-';
 
-    const referrer = req.headers.referrer || req.headers.referer;
+    const referrer = req.headers.referrer || req.headers.referer || '-';
     const userAgent = req.get('User-agent');
 
     console.log(`${uuid} ${userId} ${remoteUser} ${date} ${method} ${url} HTTP/${httpVersion} ${statusCode} ${contentLength} ${referrer} ${userAgent}`);
