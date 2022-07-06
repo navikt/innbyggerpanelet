67c5c58d3701bcad00033f11bc3348a0085e103c
Amund Lunke Røhne <43012250+amundlrohne@users.noreply.github.com>
2022-07-06 09:18:51 +0200

    feat[backend]: restrict citizens from search if exp date < end date (#120)
    * feat[backend]: restrict citizens from search if exp date < end date
    
    * feat[backend]: change query param name for prioritized citizens

--- apps/frontend/src/common/api/hooks/useCitizen.ts
+++ apps/frontend/src/common/api/hooks/useCitizen.ts
@@ -3,10 +3,14 @@ import { AxiosError } from 'axios';
 import useSWR from 'swr';
 import { fetcher } from '../operations';
 
-export const useCitizenByCriterias = (criterias: ICriteria[]) => {
-    const queryString = criterias.map((c) => `criterias[]=${c.id}`).join('&');
+export const useCitizenByCriterias = (criterias: ICriteria[], insightEndDate: string) => {
+    const criteriasQueryString = criterias.map((c) => `criterias[]=${c.id}`).join('&');
+    const insightEndDateQueryString = `&insightEndDate=${insightEndDate}`;
 
-    const { data, error } = useSWR<ICitizen[], AxiosError>(`/api/citizen/prioritized?${queryString}`, fetcher);
+    const { data, error } = useSWR<ICitizen[], AxiosError>(
+        `/api/citizen/prioritized?${criteriasQueryString + insightEndDateQueryString}`,
+        fetcher
+    );
 
     return { citizens: data, loading: !data && !error, error: error };
 };
