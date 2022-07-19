6604aed6f00f8e3ff0705f376ea0a343745b96f0
Amund Lunke Røhne <amund.lunke.rohne@nav.no>
2022-07-19 11:16:41 +0200

    feat[backend]: scheduler logging with durations

--- apps/api/src/schedulers/insightScheduler.ts
+++ apps/api/src/schedulers/insightScheduler.ts
@@ -2,18 +2,31 @@ import cron from 'node-cron';
 import { database } from '../loaders';
 import { InsightService } from './../services/InsightService';
 import { MessageService } from './../services/MessageService';
+import cronLogger, { EnumScheduleTypes } from './cronLogging';
 
 const insightExpiredNotifyEmployees = cron.schedule('0 0 * * *', async () => {
+    cronLogger.scheduleStart('Insight Expiration Notification');
+
     const dateToday = new Date().toISOString().slice(0, 10);
 
-    const insightService = new InsightService(database);
-    const insights = await insightService.search({
-        where: { end: dateToday },
-        relations: ['project', 'project.members']
-    });
+    try {
+        const insightService = new InsightService(database);
+        const insights = await insightService.search({
+            where: { end: dateToday },
+            relations: ['project', 'project.members']
+        });
 
-    const messageService = new MessageService(database);
-    const messages = await messageService.createInsightExpirationNotification(insights);
+        const messageService = new MessageService(database);
+        const messages = await messageService.createInsightExpirationNotification(insights);
+        cronLogger.scheduleStop(
+            'Insight Expiration Notification',
+            'Message',
+            messages.length,
+            EnumScheduleTypes.Create
+        );
+    } catch (error) {
+        cronLogger.scheduleInterrupted('Insight Expiration Notification');
+    }
 });
 
 const setupInsightSchedules = () => {
