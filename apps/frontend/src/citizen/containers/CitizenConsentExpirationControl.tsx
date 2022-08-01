5c2c0037d8649e7a330744a218b883572aa1f1ee
Amund Lunke Røhne <amund.lunke.rohne@nav.no>
2022-08-01 15:27:05 +0200

    feat[backend/frontend]: delete user, anonymize candidatures, trigger logout

---
+++ apps/frontend/src/citizen/containers/CitizenConsentExpirationControl.tsx
@@ -0,0 +1,55 @@
+import { ICitizen } from '@innbyggerpanelet/api-interfaces';
+import { Refresh } from '@navikt/ds-icons';
+import { Button, Panel, TextField } from '@navikt/ds-react';
+import { AxiosError } from 'axios';
+import { ReactElement, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { deleteCitizen, extendCitizenExpirationDate } from '../../common/api/mutations';
+import { APIHandler } from '../../common/components/apiHandler';
+import style from './CitizenConsentExpirationControl.module.scss';
+
+interface IProps {
+    citizen: ICitizen;
+}
+
+export const CitizenConsentExpirationControl = ({ citizen }: IProps): ReactElement => {
+    const { expirationDate } = citizen;
+
+    const [putError, setPutError] = useState<AxiosError>();
+    const [deleteError, setDeleteError] = useState<AxiosError>();
+    const navigate = useNavigate();
+
+    const handleExtendExpirationDate = async () => {
+        const { response, error } = await extendCitizenExpirationDate(citizen);
+        if (error) return setPutError(error);
+        if (response) navigate('/innbygger/profil');
+    };
+
+    const handleDeleteCitizen = async () => {
+        const { response, error } = await deleteCitizen();
+        if (error) return setDeleteError(error);
+        if (response) window.location.replace('/api/auth/logout');
+    };
+
+    return (
+        <Panel>
+            <div className={style.expiration}>
+                <TextField
+                    label="Utløpsdato"
+                    id="expirationDate"
+                    name="expirationDate"
+                    disabled={true}
+                    value={expirationDate}
+                />
+                <Button onClick={handleExtendExpirationDate}>
+                    <Refresh />
+                </Button>
+                <Button variant="danger" onClick={handleDeleteCitizen}>
+                    Slett konto
+                </Button>
+            </div>
+            {putError && <APIHandler loading={false} error={putError} />}
+            {deleteError && <APIHandler loading={false} error={deleteError} />}
+        </Panel>
+    );
+};
