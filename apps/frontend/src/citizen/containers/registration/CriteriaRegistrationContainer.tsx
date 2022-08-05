a5f0ab050503ae7352920730e1ceccaad4242a6d
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-08-05 15:20:03 +0200

    Add rendering of criterias based on exclusivity slug

--- apps/frontend/src/citizen/containers/registration/CriteriaRegistrationContainer.tsx
+++ apps/frontend/src/citizen/containers/registration/CriteriaRegistrationContainer.tsx
@@ -1,5 +1,5 @@
 import { ICriteriaCategory } from '@innbyggerpanelet/api-interfaces';
-import { Panel, Radio, RadioGroup } from '@navikt/ds-react';
+import { Checkbox, CheckboxGroup, Panel, Radio, RadioGroup } from '@navikt/ds-react';
 import { useCriteriaByCategoryId } from '../../../common/api/hooks';
 import React, { ReactElement } from 'react';
 import style from './CriteriaRegistrationContainer.module.scss';
@@ -14,11 +14,20 @@ export function CriteriaRegistrationContainer({
 
     return (
         <Panel className={style.citizenCriteriaContainer}>
-            <RadioGroup legend={criteriaCategory.name} size="medium">
-                {criterias?.map((criteria) => {
-                    return <Radio value={criteria.name}>{criteria.name}</Radio>;
-                })}
-            </RadioGroup>
+            {criterias?.[0].exclusivitySlug 
+                ?  
+                <RadioGroup legend={criteriaCategory.name} size="small">
+                    {criterias.map((criteria) => {
+                        return <Radio value={criteria.name}>{criteria.name}</Radio>;
+                    })}
+                </RadioGroup>
+                : 
+                <CheckboxGroup legend={criteriaCategory.name} size="small">
+                    {criterias?.map((criteria) => {
+                        return <Checkbox value={criteria.name}>{criteria.name}</Checkbox>;
+                    })}
+                </CheckboxGroup>
+            }
         </Panel>
     );
 }
