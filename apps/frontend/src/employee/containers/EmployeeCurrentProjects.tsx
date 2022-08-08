1dc29cfe2c99010e360251e3ede662f7a78a8866
Amund Lunke Røhne <amund.lunke.rohne@nav.no>
2022-08-08 14:47:23 +0200

    fix[frontend]: remove container bloat

--- apps/frontend/src/employee/containers/EmployeeCurrentProjects.tsx
+++ apps/frontend/src/employee/containers/EmployeeCurrentProjects.tsx
@@ -2,7 +2,6 @@ import { IInsightProject } from '@innbyggerpanelet/api-interfaces';
 import { BodyShort, Button, Heading } from '@navikt/ds-react';
 import { ReactElement } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { PanelNoBackground } from '../../common/components/panelNoBackground';
 import { EmployeeCurrentProjectCard } from '../components';
 import style from './EmployeeCurrentProjects.module.scss';
 
@@ -24,37 +23,36 @@ export const EmployeeCurrentProjects = ({
                 <Heading size="xlarge">Mine prosjekter</Heading>
                 <Button onClick={() => navigate('prosjekt/ny')}>Nytt prosjekt</Button>
             </div>
-            <PanelNoBackground>
-                <div className={style.projectsContainer}>
-                    {onGoingProjects.length !== 0 ? (
-                        onGoingProjects.map((item, i) => {
-                            return <EmployeeCurrentProjectCard key={i} insightProject={item} id={item.id} />;
-                        })
-                    ) : (
-                        <BodyShort>Du har ingen pågående prosjekter...</BodyShort>
-                    )}
-                </div>
-                <Heading size="large">Fremtidige</Heading>
-                <div className={style.projectsContainer}>
-                    {futureProjects.length !== 0 ? (
-                        futureProjects.map((item, i) => {
-                            return <EmployeeCurrentProjectCard key={i} insightProject={item} id={item.id} />;
-                        })
-                    ) : (
-                        <BodyShort>Du har ingen fremtidige prosjekter...</BodyShort>
-                    )}
-                </div>
-                <Heading size="large">Gjennomførte</Heading>
-                <div className={style.projectsContainer}>
-                    {completedProjects.length !== 0 ? (
-                        completedProjects.map((item, i) => {
-                            return <EmployeeCurrentProjectCard key={i} insightProject={item} id={item.id} />;
-                        })
-                    ) : (
-                        <BodyShort>Du har ingen gjennomførte prosjekter...</BodyShort>
-                    )}
-                </div>
-            </PanelNoBackground>
+
+            <div className={style.projectsContainer}>
+                {onGoingProjects.length !== 0 ? (
+                    onGoingProjects.map((item, i) => {
+                        return <EmployeeCurrentProjectCard key={i} insightProject={item} id={item.id} />;
+                    })
+                ) : (
+                    <BodyShort>Du har ingen pågående prosjekter...</BodyShort>
+                )}
+            </div>
+            <Heading size="large">Fremtidige</Heading>
+            <div className={style.projectsContainer}>
+                {futureProjects.length !== 0 ? (
+                    futureProjects.map((item, i) => {
+                        return <EmployeeCurrentProjectCard key={i} insightProject={item} id={item.id} />;
+                    })
+                ) : (
+                    <BodyShort>Du har ingen fremtidige prosjekter...</BodyShort>
+                )}
+            </div>
+            <Heading size="large">Gjennomførte</Heading>
+            <div className={style.projectsContainer}>
+                {completedProjects.length !== 0 ? (
+                    completedProjects.map((item, i) => {
+                        return <EmployeeCurrentProjectCard key={i} insightProject={item} id={item.id} />;
+                    })
+                ) : (
+                    <BodyShort>Du har ingen gjennomførte prosjekter...</BodyShort>
+                )}
+            </div>
         </div>
     );
 };
