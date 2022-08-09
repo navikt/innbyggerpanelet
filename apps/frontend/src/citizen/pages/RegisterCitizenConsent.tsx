bac2661f25ce1b9d4e9f2c2389f0c3b910b4aafb
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-08-09 16:48:29 +0200

    Beginnings of consent view

--- apps/frontend/src/citizen/pages/RegisterCitizenConsent.tsx
+++ apps/frontend/src/citizen/pages/RegisterCitizenConsent.tsx
@@ -1,6 +1,10 @@
 import { ICitizen } from '@innbyggerpanelet/api-interfaces';
+import { HandsHeart } from '@navikt/ds-icons';
+import { Heading, Panel } from '@navikt/ds-react';
 import React, { ReactElement } from 'react';
 import { useLocation } from 'react-router-dom';
+import { ConsentTextPanel } from '../components/ConsentTextPanel';
+import style from './RegisterCitizenConsent.module.scss';
 
 export const RegisterCitizenConsent = (): ReactElement => {
     
@@ -9,6 +13,35 @@ export const RegisterCitizenConsent = (): ReactElement => {
 
 
     return (
-        <div>heihei</div>
+        <>
+            <Panel className={style.headerContainer}>
+                <div className={style.heartIconContainer}>
+                    <HandsHeart height='2rem' width='2rem'/>
+                </div>
+                <Heading size="large" className={style.consentHeader}>
+                    Samtykke til registrering i Innbyggerpanelet
+                </Heading>
+            </Panel>
+            <ConsentTextPanel 
+                header='Hva er Innbyggerpanelet?'
+                paragraphs={[
+                    `For å levere gode tjenester er NAV avhengig av å involvere og 
+                    få tilbakemelding fra Navs brukere.`,
+                    `Som frivillig deltager i Innbyggerpanelet har du muligheten til 
+                    å gi dine tilbakemeldinger og innspill på Navs løsninger og tjenester.`,
+                    `Hvis du velger å registrere deg, kan du bli kontaktet for å delta på 
+                    konkrete innsiktarbeid. Løsningen håndterere kommunikasjonen og 
+                    samtykke til dette. Opplysningene du oppgir under denne registeringen, 
+                    brukes kun for å finne relevante løsninger og tjenester du kan gi innspill til.`
+                ]}
+            />
+            <ConsentTextPanel 
+                header='Hvem har ansvar for Innbyggerpanelet?'
+                paragraphs={[
+                    `NAV Arbeid- og velferdsdirektoratet er behandlingsansvarlig for Innbyggerpanelet. 
+                    Innbyggerpanelet driftes av IT-avdelingen til Arbeid- og velferdsdirektoratet (NAV). `
+                ]}
+            />
+        </>
     );
 };
