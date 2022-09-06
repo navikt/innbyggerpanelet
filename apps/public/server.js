de85e68b078937990a6248ba3d61b5bdb306bdd2
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-09-06 21:04:28 +0200

    Update server

--- apps/public/server.js
+++ apps/public/server.js
@@ -7,7 +7,9 @@ const port = 3000
 
 dotenv.config()
 
-app.use(express.static(path.join(__dirname, '../../dist/apps/public')))
+const buildPath = path.resolve(__dirname, '../../dist/apps/public')
+
+app.use(process.env.BASE_PATH, express.static(buildPath, {index: false}))
 
 app.get(`${process.env.BASE_PATH}/isalive`, (req, res) => {
     res.sendStatus(200);
@@ -17,9 +19,7 @@ app.get(`${process.env.BASE_PATH}/isready`, (req, res) => {
     res.sendStatus(200);
 });
 
-app.get(`${process.env.BASE_PATH}/`, (req, res) => {
-    res.sendFile(path.join(__dirname, '../../dist/apps/public', `${process.env.BASE_PATH}/index.html`))
-})
+app.use(/^(?!.*\/(internal|static)\/).*$/, `${buildPath}/index.html`)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
