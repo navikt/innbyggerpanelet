5f6c3cc87fcf80608e792e44a1527db89f1af182
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-09-06 19:51:47 +0200

    Add new apps

--- apps/frontend/src/environments/environment.ts
+++ apps/frontend/src/environments/environment.ts
@@ -3,4 +3,15 @@
 
 export const environment = {
     production: false,
+    azureAd: {
+        clientId: process.env.AZURE_APP_CLIENT_ID,
+        secret: process.env.AZURE_APP_CLIENT_SECRET,
+        tenantId: process.env.AZURE_APP_TENANT_ID
+    },
+    idPorten: {
+        clientId: process.env.IDPORTEN_CLIENT_ID,
+        jwk: JSON.parse(process.env.IDPORTEN_CLIENT_JWK!),
+        redirectUri: process.env.IDPORTEN_REDIRECT_URI,
+        wellKnown: process.env.IDPORTEN_WELL_KNOWN_URL
+    }
 };
---
+++ apps/public/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+
+const app = express()
+const port = 3000
+
+dotenv.config()
+
+app.use(express.static(path.join(__dirname, '../../dist/apps/public')))
+
+app.get(`${process.env.BASE_PATH}/isalive`, (req, res) => {
+    res.sendStatus(200);
+});
+
+app.get(`${process.env.BASE_PATH}/isready`, (req, res) => {
+    res.sendStatus(200);
+});
+
+app.get(`${process.env.BASE_PATH}/`, (req, res) => {
+    res.sendFile(path.join(__dirname, '../../dist/apps/public', 'index.html'))
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
