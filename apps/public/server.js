73ae73b8a77d3389fcde2809ae26f4a34356d41f
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-09-06 20:45:16 +0200

    Update to es5 for server

--- apps/public/server.js
+++ apps/public/server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import path from 'path';
-import dotenv from 'dotenv';
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
 
 const app = express()
 const port = 3000
