b58f1afe323de4de3a9674d7b027cac4cf4699bd
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-09-13 12:44:18 +0200

    Try withouth base path

--- apps/public/server.js
+++ apps/public/server.js
@@ -7,7 +7,7 @@ const port = 3000
 
 dotenv.config()
 
-app.use(process.env.BASE_PATH, express.static(path.join(__dirname, '../../dist/apps/public')))
+app.use(express.static(path.join(__dirname, '../../dist/apps/public')))
 
 app.get(`${process.env.BASE_PATH}/isalive`, (req, res) => {
     res.sendStatus(200);
@@ -17,7 +17,7 @@ app.get(`${process.env.BASE_PATH}/isready`, (req, res) => {
     res.sendStatus(200);
 });
 
-app.get(`${process.env.BASE_PATH}/`, (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../dist/apps/public', 'index.html'))
 })
 
