3b6965df0228b67402c1363c11fcb3f3c0ffe249
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-09-14 11:33:55 +0200

    Deploy vite test

---
+++ vite-test/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const expressStaticGzip = require('express-static-gzip');
+
+dotenv.config()
+
+const basePath = process.env.BASE_PATH
+const buildPath = path.join(path.resolve(__dirname, './dist'))
+const app = express()
+const port = 3000
+
+app.use(
+    basePath,
+    expressStaticGzip(`${buildPath}`, {
+        enableBrotli: true,
+        orderPreference: ['br']
+    })
+);
+
+app.get(`${basePath}/isAlive|${basePath}/isReady`, (req, res) => {
+    res.send('OK')
+})
+
+
+app.listen(port, () => { console.log('listening on port 3000')})
