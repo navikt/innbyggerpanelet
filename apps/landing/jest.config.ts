5382009ce3f91a6496b940879c9e3d1e19e329f3
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-09-14 10:24:11 +0200

    Update naming

--- apps/landing/jest.config.ts
+++ apps/landing/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-    displayName: 'public',
+    displayName: 'landing',
     preset: '../../jest.preset.js',
     transform: {
         '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
         '^.+\\.[tj]sx?$': 'babel-jest'
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    coverageDirectory: '../../coverage/apps/public'
+    coverageDirectory: '../../coverage/apps/landing'
 };
--- apps/landing/src/app/app.tsx
+++ apps/landing/src/app/app.tsx
@@ -1,11 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
 export function App() {
-    return (
-        <>
-            Hello
-        </>
-    );
+    return <>Hello</>;
 }
 
 export default App;
