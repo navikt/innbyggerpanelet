661b039d770f6ccb422ffd6e1fb7d13cbf149d7b
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-09-26 14:01:54 +0200

    Add base to citizen app

--- employee/vite.config.ts
+++ employee/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    define: {
+        'process.env': process.env,
+    },
+    plugins: [react(), eslintPlugin()],
+    base: '/innbyggerpanelet/ansatt',
 })
