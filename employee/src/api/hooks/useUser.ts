acb8ddb8406d164d7e08fec79e6dcd494dbef303
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-10-11 14:19:07 +0200

    I am big dumb dumb

--- employee/src/api/hooks/useUser.ts
+++ employee/src/api/hooks/useUser.ts
@@ -5,7 +5,7 @@ import { fetcher } from '../operations'
 
 // Gets currently authenticated user
 export const useUser = <T>() => {
-    fetch('/innbyggerpanelet/innbygger/api/candidate')
+    fetch('/innbyggerpanelet/ansatt/api/candidate')
     const { data, error } = useSWR<T, AxiosError>('/innbyggerpanelet/ansatt/api/candidate', fetcher)
 
     return { user: data as T, loading: !data && !error, error }
