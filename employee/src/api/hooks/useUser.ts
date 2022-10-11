e58de79a1f9a6dbd1ba46ee97ed82c9a80cdff99
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-10-11 14:08:02 +0200

    Update useUser.ts

--- employee/src/api/hooks/useUser.ts
+++ employee/src/api/hooks/useUser.ts
@@ -5,7 +5,7 @@ import { fetcher } from '../operations'
 
 // Gets currently authenticated user
 export const useUser = <T>() => {
-    const { data, error } = useSWR<T, AxiosError>('/api/candidate/', fetcher)
+    const { data, error } = useSWR<T, AxiosError>('/innbyggerpanelet/ansatt/api/candidate/', fetcher)
 
     return { user: data as T, loading: !data && !error, error }
 }
