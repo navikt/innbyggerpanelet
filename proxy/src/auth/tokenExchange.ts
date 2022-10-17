97ec5eb8809779b92e18aa49eabd2fabe8303562
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-10-17 15:27:12 +0200

    Try manual string

--- proxy/src/auth/tokenExchange.ts
+++ proxy/src/auth/tokenExchange.ts
@@ -38,7 +38,7 @@ export default class TokenExchangeClient {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 authorization: `Bearer ${accessToken}`,
             },
-            body: new URLSearchParams(Object.entries(tokenOptions)).toString(),
+            body: `grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&client_id=${config.azureAd.clientId}&client_secret=${config.azureAd.secret}&assertion=${accessToken}&scope=api://${config.app.targetAudience}/.default&requested_token_use=on_behalf_of`,
         })
             .then((tokenSet: any) => {
                 console.log(tokenSet)
