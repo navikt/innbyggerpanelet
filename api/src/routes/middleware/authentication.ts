7cf8888a8d04fd96b4de2e53e1624879859e6450
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-10-18 15:42:58 +0200

    Start work on new user creation in auth

--- api/src/routes/middleware/authentication.ts
+++ api/src/routes/middleware/authentication.ts
@@ -3,10 +3,11 @@ import * as jose from 'jose'
 import { UnathorizedError } from '../../lib/errors/http/UnauthorizedError'
 import { ServerErrorMessage } from '../../lib/errors/messages/ServerErrorMessages'
 import config from '../../config'
-import { EmployeeService } from '../../services'
+import { CitizenService, EmployeeService } from '../../services'
 import { database } from '../../loaders'
 import { Employee } from '../../models/employee/EmployeeEntity'
 import { EnumUserRole } from '../../types'
+import { Citizen } from '../../models/citizen/CitizenEntity'
 
 export const checkAuth = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -54,7 +55,7 @@ export const checkAzureAuth = async (req: Request, res: Response, next: NextFunc
                 return user
             })
             .catch(async (error) => {
-                // TODO: check azureAD group for team-researchops
+                // TODO: update azureAD group depending on dev/prod
                 return await employeeService.create({
                     id: payload.oid as string,
                     firstname: (payload.name as string).split(',')[1],
@@ -68,12 +69,113 @@ export const checkAzureAuth = async (req: Request, res: Response, next: NextFunc
                     messages: [],
                 })
             })
-        console.log(await employeeService.getById(payload.oid as string))
     } catch (err) {
         next(err)
     }
     next()
 }
-// 1. set up auth check for Azure OBO
-// 2. set up user creation for Azure
-// 3. set up user creation for ID-porten
+
+// if isuer == azure -> employee, else issuer == tokenX -> citizen
+
+export interface IReqWithUserPermissions extends Request {
+    user: {
+        role: EnumUserRole
+        id: string
+    }
+}
+
+// TODO: Add this instead of auth check above
+const addUserDetailsToRequest = async (req: IReqWithUserPermissions, res: Response, next: NextFunction) => {
+    const bearerSchema = req.headers.authorization
+    if (!bearerSchema) throw new UnathorizedError({ message: ServerErrorMessage.noBearerSchema() })
+
+    const token = bearerSchema.split(' ')[1]
+    if (!token) throw new UnathorizedError({ message: ServerErrorMessage.noTokenInSchema() })
+
+    try {
+        const { payload, protectedHeader } = await jose.jwtVerify(
+            token,
+            jose.createRemoteJWKSet(new URL(process.env.AZURE_OPENID_CONFIG_JWKS_URI!)),
+            {
+                issuer: process.env.AZURE_OPENID_CONFIG_ISSUER,
+                audience: process.env.AZURE_APP_CLIENT_ID,
+            },
+        )
+
+        if (!payload || !protectedHeader) throw new Error('Not azure authenticated')
+
+        // Create employee if not exists
+        const employeeService = new EmployeeService(database)
+        const employee: Employee = await employeeService
+            .getById(payload.oid as string)
+            .then((user) => {
+                return user
+            })
+            .catch(async (error) => {
+                // TODO: update azureAD group depending on dev/prod
+                return await employeeService.create({
+                    id: payload.oid as string,
+                    firstname: (payload.name as string).split(',')[1],
+                    surname: (payload.name as string).split(',')[0],
+                    email: payload.preferred_username as string,
+                    role: (payload.groups as Array<string>).includes('2d7f1c0d-5784-4f81-8bb2-8f3a79f8f949')
+                        ? EnumUserRole.Admin
+                        : EnumUserRole.InsightWorker,
+                    registered: true,
+                    insightProjects: [],
+                    messages: [],
+                })
+            })
+
+        // Add employee to req
+        req.user.id = employee.id
+        req.user.role = employee.role
+    } catch (error) {
+        // Not azure authenticated
+        // TODO: throw errors given by employee service
+    }
+
+    try {
+        const { payload, protectedHeader } = await jose.jwtVerify(
+            token,
+            jose.createRemoteJWKSet(new URL(config.tokenX.jwksURI!)),
+            {
+                issuer: config.tokenX.issuer,
+                audience: config.tokenX.clientId,
+            },
+        )
+
+        if (!payload || !protectedHeader) throw new Error('Not idporten authenticated')
+
+        // Create citizen if not exists
+        const citizenService = new CitizenService(database)
+        const citizen: Citizen = await citizenService
+            .getById(payload.sub as string)
+            .then((user) => {
+                return user
+            })
+            .catch(async (error) => {
+                return await citizenService.create({
+                    id: payload.sub as string,
+                    pnr: payload.pid as string,
+                    firstname: '',
+                    surname: '',
+                    phone: '',
+                    registered: false,
+                    role: EnumUserRole.Citizen,
+                    expirationDate: new Date(new Date().setFullYear(new Date().getFullYear() + 1)).toISOString(),
+                    candidates: [],
+                    criterias: [],
+                    messages: [],
+                })
+            })
+
+        req.user.id = citizen.id
+        req.user.role = citizen.role
+    } catch (error) {
+        // Not IDporten authenticated
+        // TODO: throw errors given by citizen service
+    }
+
+    // TODO: throw error if not azuer or idporten authenticated
+}
