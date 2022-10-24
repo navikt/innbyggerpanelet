4f32603db90063b248360ec879710e80ce0d2c4f
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-10-24 16:28:47 +0200

    Update EmployeeLandingPage.tsx

--- employee/src/pages/employeeLanding/EmployeeLandingPage.tsx
+++ employee/src/pages/employeeLanding/EmployeeLandingPage.tsx
@@ -2,14 +2,14 @@ import React, { ReactElement } from 'react'
 import { PageHeader } from '../../components/common/pageHeader/PageHeader'
 import { People } from '@navikt/ds-icons'
 import { useUser } from '../../api/hooks/useUser'
-import { ICitizen } from '../../types'
+import { ICitizen, IEmployee } from '../../types'
 import { useFilterInsightProjects } from './hooks/useEmployeeLanding'
 import style from './EmployeeLandingPage.module.scss'
 import { PanelNoBackground } from '../../components/common/panelNoBackground/PanelNoBackground'
 import { EmployeeCurrentProjects } from '../../container/employeeCurrentProjects/EmployeeCurrentProjects'
 
 export const EmployeeLandingPage = (): ReactElement => {
-    const { user, loading, error } = useUser<ICitizen>()
+    const { user, loading, error } = useUser<IEmployee>()
 
     const { onGoingProjects, futureProjects, completedProjects } = useFilterInsightProjects()
     console.log(user)
