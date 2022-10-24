e3b973615f911d4e31d50148b914f99bdca641ed
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-10-24 16:46:05 +0200

    Update proxy

--- employee/src/pages/employeeLanding/EmployeeLandingPage.tsx
+++ employee/src/pages/employeeLanding/EmployeeLandingPage.tsx
@@ -13,7 +13,7 @@ export const EmployeeLandingPage = (): ReactElement => {
 
     const { onGoingProjects, futureProjects, completedProjects } = useFilterInsightProjects()
     console.log(user)
-    console.log(user.firstname)
+
     return (
         <>
             <PageHeader title={user?.firstname + ' ' + user?.surname} icon={<People />} />
