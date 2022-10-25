a494e57ddca3489162e275e8d8d085b5ce9a9f5d
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-10-25 16:26:40 +0200

    Update API error handler

--- api/src/routes/middleware/errorHandler.ts
+++ api/src/routes/middleware/errorHandler.ts
@@ -7,21 +7,39 @@ import { BadRequestError } from '../../lib/errors/http/BadRequestError'
 import { ServerErrorMessage } from '../../lib/errors/messages/ServerErrorMessages'
 import { InvalidTokenError } from 'jwt-decode'
 import { UnathorizedError } from '../../lib/errors/http/UnauthorizedError'
+import { IErrorResponse } from '../../lib/errors/IErrorResponse'
 
 export function errorHandler(error: Error, request: Request, response: Response, next: NextFunction) {
+    // if (error instanceof BaseError) {
+    //     return response.status(error.httpStatus).json(error.toResponse())
+    // }
+
+    // if (error instanceof QueryFailedError) {
+    //     const error = new BadRequestError({ message: ServerErrorMessage.invalidQuery() })
+    //     return response.status(error.httpStatus).json(error.toResponse())
+    // }
+
+    // if (!config.backend.prod)
+    //     return response
+    //         .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    //         .json(`Something went wrong.. Try again later. \n${error}`)
+
+    // return response.status(500).json('Something went wrong.. Try again later.')
+    let errorStatus: number = StatusCodes.INTERNAL_SERVER_ERROR
+    let errorMessage: IErrorResponse | string = config.backend.prod
+        ? `Something went wrong.. Try again later. \n${error}`
+        : 'Something went wrong.. Try again later.'
+
     if (error instanceof BaseError) {
-        return response.status(error.httpStatus).json(error.toResponse())
+        errorStatus = error.httpStatus
+        errorMessage = error.toResponse()
     }
 
     if (error instanceof QueryFailedError) {
-        const error = new BadRequestError({ message: ServerErrorMessage.invalidQuery() })
-        return response.status(error.httpStatus).json(error.toResponse())
+        const badRequestError = new BadRequestError({ message: ServerErrorMessage.invalidQuery() })
+        errorStatus = badRequestError.httpStatus
+        errorMessage = badRequestError.toResponse()
     }
 
-    if (!config.backend.prod)
-        return response
-            .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .json(`Something went wrong.. Try again later. \n${error}`)
-
-    return response.status(504).json('Something went wrong.. Try again later.')
+    response.status(errorStatus).json(errorMessage)
 }
